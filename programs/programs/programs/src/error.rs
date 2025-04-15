f29f4dbcff00ff75821fb6a63f8c0edf99fd78d5
Lothar <lejeune.py@gmail.com>
2025-04-15 07:51:21 +0200

    feat: create epoch and token proposal (#31)

--- programs/programs/programs/src/error.rs
+++ programs/programs/programs/src/error.rs
@@ -2,6 +2,15 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum ErrorCode {
-    #[msg("Custom error message")]
+    #[msg("L'allocation du créateur ne peut pas dépasser 10%")]
+    CreatorAllocationTooHigh,
+    
+    #[msg("L'époque n'est pas active")]
+    EpochNotActive,
+    
+    #[msg("La plage de temps de l'époque est invalide")]
+    InvalidEpochTimeRange,
+    
+    #[msg("Erreur personnalisée")]
     CustomError,
 }
--- programs/programs/programs/src/instructions/initialize.rs
+++ programs/programs/programs/src/instructions/initialize.rs
@@ -3,6 +3,6 @@ use anchor_lang::prelude::*;
 #[derive(Accounts)]
 pub struct Initialize {}
 
-pub fn handler(ctx: Context<Initialize>) -> Result<()> {
+pub fn handler(_ctx: Context<Initialize>) -> Result<()> {
     Ok(())
 }
