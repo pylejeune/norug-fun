d29732495922e3c26d1d338dba279b43052344c7
Pierre-Yves Lejeune <lejeune.py@gmail.com>
2025-04-23 00:34:03 +0200

    chore(program): add program id on devnet

--- programs/programs/programs/src/lib.rs
+++ programs/programs/programs/src/lib.rs
@@ -12,7 +12,7 @@ pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("89S48feUon6ffgtLzsnqoBVwdb1mxT4rmhRR5WnYefpA");
+declare_id!("3HBzNutk8DrRfffCS74S55adJAjgY8NHrWXgRtABaSbF");
 
 
 #[program]
