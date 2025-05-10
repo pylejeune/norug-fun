88beedde016a1229184e5de435c34b2833a729a2
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2025-05-10 13:47:07 +0200

    feat(profil): create profil user page

---
+++ front/components/profile/ProfileImage.tsx
@@ -0,0 +1,27 @@
+import defaultProfileImage from "@/public/images/default-profile.png";
+import Image from "next/image";
+
+type ProfileImageProps = {
+  size?: number;
+  className?: string;
+};
+
+export default function ProfileImage({
+  size = 80,
+  className = "",
+}: ProfileImageProps) {
+  return (
+    <div
+      className={`relative rounded-full overflow-hidden bg-gray-800 ${className}`}
+      style={{ width: size, height: size }}
+    >
+      <Image
+        src={defaultProfileImage}
+        alt="Profile"
+        fill
+        className="object-cover"
+        priority
+      />
+    </div>
+  );
+}
