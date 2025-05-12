de6e4d19787742f0ef2a70777045c0bfc501f097
Pierre-Yves Lejeune <lejeune.py@gmail.com>
2025-05-12 22:53:40 +0200

    chore: clean useless file

--- front/app/api/hello/route.js
+++ front/app/api/hello/route.js
@@ -1,5 +1,5 @@
 export async function GET(request) {
-    return new Response(JSON.stringify({ message: 'Bonjour depuis App Router' }), {
+    return new Response(JSON.stringify({ message: 'Bienvenue sur noRug.fun' }), {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
     });
